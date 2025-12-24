63eff23ddc7b4758cb8f56c27b743d32b874c7a8
Mohamed <mohamedabuali97@gmail.com>
2025-12-23 22:33:35 -0600

    style(ui): improve directory list layout and styling
    - Update grid layout and dimensions in App.css
    - Add styling for directory items and container
    - Implement horizontal scrollable directory list

--- src/componenets/DirList.tsx
+++ src/componenets/DirList.tsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const DirList = () => {
   return (
-    <div className='dirlist-container'>DirList</div>
+    <div className='dirlist-container'>
+        <ul className='dir-list'>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                <li className='dir-item'>
+
+                </li>
+                
+        </ul>
+    </div>
   )
 }
 
