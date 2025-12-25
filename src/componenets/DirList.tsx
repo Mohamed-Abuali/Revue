cb49559a08c07788518aa3a5246b09e4f7f52d57
Mohamed <mohamedabuali97@gmail.com>
2025-12-24 23:38:30 -0600

    feat(DirList): implement dynamic folder listing from project structure
    Add projectStructure constant and map it to render folder items dynamically instead of hardcoded empty list items. This makes the component more maintainable and adaptable to actual project structure.

--- src/componenets/DirList.tsx
+++ src/componenets/DirList.tsx
@@ -4,30 +4,13 @@ const DirList = () => {
     <div className='dirlist-container'>
       <h2>Folders</h2>
         <ul className='dir-list'>
-                <li className='dir-item'>
-
+          {projectStructure.map((dir,i) => (
+             <li key={i} className='dir-item'>
+                    {dir.toString()}\
                 </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
-                <li className='dir-item'>
-
-                </li>
+          ))}
+               
+                
                 
         </ul>
     </div>
@@ -35,3 +18,53 @@ const DirList = () => {
 }
 
 export default DirList
+
+const projectStructure = [{
+  src: {
+    components: {
+      atoms: {},
+      molecules: {},
+      organisms: {},
+      templates: {},
+      pages: {}
+    },
+    pages: {},
+    hooks: {},
+    contexts: {},
+    services: {},
+    utils: {},
+    styles: {},
+    assets: {
+      images: {},
+      fonts: {},
+      icons: {}
+    },
+    lib: {
+      tools: {},
+      functions: {},
+      constants: {},
+      types: {}
+    },
+    routes: {},
+    store: {},
+    config: {}
+  },
+  public: {
+    index: {},
+    favicon: {},
+    manifest: {}
+  },
+  tests: {
+    unit: {},
+    integration: {},
+    e2e: {}
+  },
+  docs: {},
+  scripts: {},
+  dist: {},
+  build: {},
+  node_modules: {},
+  '.git': {},
+  '.vscode': {},
+  '.github': {}
+}];
