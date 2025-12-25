c2b5d7595935bfb78bc8eb8652258d6dc9cc114d
Mohamed <mohamedabuali97@gmail.com>
2025-12-24 23:22:04 -0600

    feat(components): add Button component and integrate into SideBar
    Add new Button component and update SideBar to include it along with a title

--- src/componenets/SideBar.tsx
+++ src/componenets/SideBar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button'
 
 const SideBar = () => {
   return (
-    <div className='sidebar-container'>SideBar</div>
+    <div className='sidebar-container'>
+      <div>
+        <h1>Revue</h1>
+      </div>
+      <div>
+        <Button/>
+      </div>
+    </div>
   )
 }
 
