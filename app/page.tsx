ae24f79f227c349de7ff6d263b0d6c24d009fbb1
Mohamed <mohamedabuali97@gmail.com>
2026-01-12 00:16:27 -0600

    style(ErrorList): improve layout and alignment of error list component

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,7 @@ export default function Home() {
       <SideBar/>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <DicList/>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          
-        </div>
+        
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
   
 
